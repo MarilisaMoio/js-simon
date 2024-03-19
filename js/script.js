96615fef2921c0e7ed016bf1c105ead6247d8ff3
Marilisa Moio <moiomarilisa@gmail.com>
2024-03-19 14:09:56 +0100

    feat: add working simon says

--- js/script.js
+++ js/script.js
@@ -2,8 +2,29 @@
 //Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 //Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 const container = document.querySelector(".container");
+const btn = document.querySelector("button")
 
-container.innerHTML = generateRandomNumbers(5, 20).join(" ");
+btn.addEventListener("click", function(){
+    const simonArray = generateRandomNumbers(5, 20);
+    container.innerHTML = simonArray.join(" ");
+    //separto in due timeout diversi per generare un delay in modo tale che il prompt non venisse triggerato prima della sparizione dei numeri originali
+    setTimeout(() => container.innerHTML = "", 2000)
+    //secondo timeout, per raccogliere i numeri dell'utente
+    setTimeout(function(){
+        let userArray = [];
+        for(let i = 1; i <= 5; i++){
+            let userNumber = prompt(`Scrivi il ${i}° numero che ricordi`);
+            userArray.push(userNumber)  
+        }
+        //comparazione del contenuto degli array, con sort per evitare che l'ordine dei numeri abbia peso
+        if (userArray.sort().toString() === simonArray.sort().toString()){
+            alert(`Hai vinto! I numeri erano ${simonArray.join(" ")} e tu li hai indovinati tutti!`);
+        } else {
+            alert(`Hai perso. I numeri erano ${simonArray.join(" ")} e tu hai invece scritto ${userArray.join(" ")}.`)
+        }
+    }, 2500)
+})
+
 
 //FUNCTION
 
